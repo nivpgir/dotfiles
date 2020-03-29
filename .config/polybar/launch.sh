3ae7bbeb015753793071e13e3ea6b2cadaea3d71
Niv Pagir <niv.pagir@orcam.com>
2020-03-29 16:10:26 +0300

    polybar dual monitor configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar-example.log
+echo "---" | tee -a /tmp/polybar-mon1.log
+echo "---" | tee -a /tmp/polybar-mon2.log
 # polybar example >> /tmp/polybar-example.log 2>&1 &
-$HOME/.local/bin/polybar example >> /tmp/polybar-example.log 2>&1 &
+$HOME/.local/bin/polybar mon1 >> /tmp/polybar-mon1.log 2>&1 &
+$HOME/.local/bin/polybar mon2 >> /tmp/polybar-mon2.log 2>&1 &
 echo "Bars launched..."
