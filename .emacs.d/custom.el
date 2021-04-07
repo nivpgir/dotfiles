47c53309aac5ee2c305eaba99b760ea0bb5f6db3
nivpgir <nivpgir@gmail.com>
2021-04-07 22:17:28 +0300

    update custom.el so it would stop annoying me

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,10 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("02591317120fb1d02f8eb4ad48831823a7926113fa9ecfb5a59742420de206e0" default)))
- '(inhibit-startup-screen t)
- '(lsp-solargraph-use-bundler t))
+   '("02591317120fb1d02f8eb4ad48831823a7926113fa9ecfb5a59742420de206e0" default))
+ '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
