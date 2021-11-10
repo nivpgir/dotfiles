91a5e1452a906c75cb3e9b2902208fb82e2146d6
cadiv <niv@cadysolutions.com>
2021-11-10 14:21:05 +0200

    emacs - support for just

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -889,3 +889,13 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+(use-package just-mode
+  :straight t)
+
+(use-package justl
+  :straight (justl
+	     :type git
+	     :host github
+	     :repo "psibi/justl\.el")
+  :custom
+  (justl-executable "c:/Users/Niv/.local/scoop/shims/just.exe"))
