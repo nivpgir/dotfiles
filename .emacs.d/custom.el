7ef166b5c0af88d07b3d939678c217bd4d78dac8
nivpgir <Nivpgir@gmail.com>
2021-11-19 13:18:58 +0200

    emacs - custom.el changes

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("02591317120fb1d02f8eb4ad48831823a7926113fa9ecfb5a59742420de206e0" default))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(lsp-rust-analyzer-server-command "rust-analyzer" nil nil "Customized with use-package rustic")
+ '(warning-suppress-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
