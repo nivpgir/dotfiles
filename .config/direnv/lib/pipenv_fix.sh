917db14ae1ed2540a047f7f7f6153e65efe52a35
nivpgir <Nivpgir@gmail.com>
2021-11-27 02:03:08 +0200

    direnv - `layout pipenv` workaround on windows

---
+++ .config/direnv/lib/pipenv_fix.sh
@@ -0,0 +1,27 @@
+# Usage: layout pipenv
+#
+# Similar to layout_python, but uses Pipenv to build a
+# virtualenv from the Pipfile located in the same directory.
+#
+layout_pipenv() {
+  PIPENV_PIPFILE="${PIPENV_PIPFILE:-Pipfile}"
+  if [[ ! -f "$PIPENV_PIPFILE" ]]; then
+    log_error "No Pipfile found.  Use \`pipenv\` to create a \`$PIPENV_PIPFILE\` first."
+    exit 2
+  fi
+
+  # VIRTUAL_ENV=$(pipenv --venv 2>/dev/null  | tr -d '\r\n' ; true)
+  VIRTUAL_ENV=$(cygpath $(pipenv --venv 2>/dev/null ; true))
+  echo VIRTUAL_ENV is $VIRTUAL_ENV
+
+  if [[  $PIPENV_ACTIVE -ne 1 ]] && [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+    pipenv install --dev
+    # VIRTUAL_ENV=$(pipenv --venv ; true)
+    VIRTUAL_ENV=$(cygpath $(pipenv --venv 2>/dev/null ; true))
+  fi
+
+  export PIPENV_ACTIVE=1
+  PATH_add "$VIRTUAL_ENV/bin"
+  export VIRTUAL_ENV
+}
+
