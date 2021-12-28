c7b1d42661875a4a08f80286a72f3d06e60cb0aa
cadiv <niv@cadysolutions.com>
2021-12-28 15:51:31 +0200

    add kmonad startup scripts for windows

---
+++ .config/kmonad/run-kmonad.cmd
@@ -0,0 +1,1 @@
+powerShell -windowstyle hidden "C:/Users/Niv/.config/kmonad/run-kmonad.ps1" >> "C:/Users/Niv/.config/kmonad/log.txt"
---
+++ .config/kmonad/run-kmonad.ps1
@@ -0,0 +1,2 @@
+# when inkoking in task scheduler use: "-windowstyle hidden -File /path/to/script
+Invoke-Expression "& `"C:/Users/Niv/.local/bin/kmonad.exe`" -c c:/Users/Niv/.config/kmonad/config.kbd"
