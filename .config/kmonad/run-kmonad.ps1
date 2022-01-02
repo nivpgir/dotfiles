01743b19eff17c10ecb80f052e306acb1cf25653
nivpgir <Nivpgir@gmail.com>
2022-01-03 00:32:35 +0200

    kmonad - more keybinds!!!

--- .config/kmonad/run-kmonad.ps1
+++ .config/kmonad/run-kmonad.ps1
@@ -1,2 +1,2 @@
 # when inkoking in task scheduler use: "-windowstyle hidden -File /path/to/script
-Invoke-Expression "& `"C:/Users/Niv/.local/bin/kmonad.exe`" -c c:/Users/Niv/.config/kmonad/config.kbd"
+Invoke-Expression "& `"C:/Users/Niv/.local/bin/kmonad.exe`" c:/Users/Niv/.config/kmonad/config.kbd"
