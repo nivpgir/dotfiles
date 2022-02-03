95a2b5c823ebff146e5719b89272fe26799d8164
nivpgir <Nivpgir@gmail.com>
2022-02-03 02:15:00 +0200

    WIP - make kmonad and komorebi work together

--- .config/kmonad/service-kmonad.ps1
+++ .config/kmonad/service-kmonad.ps1
@@ -21,8 +21,8 @@ $processOptions = @{
   FilePath = "kmonad.exe"
   WindowStyle = "hidden"
   ArgumentList = "C`:/Users/Niv/.config/kmonad/config.kbd","-l","debug","-c"
-  # Verb = "RunAs"
-  RedirectStandardOutput = "config.kbd.log"
+  Verb = "RunAs"
+  # RedirectStandardOutput = "config.kbd.log"
 }
 echo ----
 echo $processOptions
---
+++ .config/komorebi/komorebi.init.ps1
@@ -0,0 +1,7 @@
+
+
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Niv\.config\komorebi'
+
+komorebic start
+
+./komorebi-configure.sh
