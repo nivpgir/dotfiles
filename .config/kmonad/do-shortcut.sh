2c9de223cbe9a1e631d6a75f4365e3c9c22be3dc
cadiv <niv@cadysolutions.com>
2022-02-12 10:09:48 +0200

    starting to add shell scripts for crossplatformness of configs

---
+++ .config/kmonad/do-shortcut.sh
@@ -0,0 +1,45 @@
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+export DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+echo $DIR-$(date) | tee ./tst.txt
+
+function terminal(){
+    alacritty
+}
+
+function web-browser(){
+    firefox.exe
+}
+
+function window-manager-restart(){
+    powershell -File $DIR/../komorebi/komorebi.init.ps1
+}
+
+function window-manager-control(){
+    komorebic $@
+}
+
+function window-manager-config(){
+    $DIR/../komorebi/komorebi-configure.sh
+}
+
+
+function kill-kmonad(){
+    powershell -File $DIR/service-kmonad.ps1 -Action kill-kmonad
+}
+
+function restart-kmonad(){
+    powershell -File $DIR/service-kmonad.ps1 -Action restart-kmonad
+}
+
+echo HIIII
+CMD=$1
+shift
+$CMD $@
