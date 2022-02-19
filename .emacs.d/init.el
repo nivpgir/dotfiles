33a051bfa4b7b047377057368426eb7a0d29cb40
nivpgir <Nivpgir@gmail.com>
2022-02-19 18:52:56 +0200

    remove rust inlay hints cause they were annoying me

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -646,7 +646,7 @@
   (lsp-rust-analyzer-cargo-watch-command "clippy")
   (lsp-eldoc-render-all t)
   (lsp-idle-delay 0.6)
-  (lsp-rust-analyzer-server-display-inlay-hints t)
+  ;; (lsp-rust-analyzer-server-display-inlay-hints t)
   (lsp-lens-enable t)
   (lsp-signature-auto-activate nil)
   )
