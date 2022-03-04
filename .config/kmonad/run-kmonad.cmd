410f61eadb0c91dc17ff055d417bd23784310db7
nivpgir <Nivpgir@gmail.com>
2022-03-04 16:02:28 +0200

    delete various script files that were migrated to justfiles

--- .config/kmonad/run-kmonad.cmd
+++
@@ -1,1 +0,0 @@
-powerShell -file "C:/Users/Niv/.config/kmonad/service-kmonad.ps1" -action start-kmonad > "C:/Users/Niv/.my-log.txt"
--- .config/my-scripts/bash.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-echo %date%%time% %* >> C:/Users/Niv/.my-log.txt
-start /w /b /d "C:/Users/Niv/.config/kmonad" "" %WINBASH% --noprofile %* >> C:/Users/Niv/.my-log.txt""
