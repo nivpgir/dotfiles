6dca44bf85e33f585005d50fe119269ccb8c2283
cadiv <niv@cadysolutions.com>
2022-04-10 14:12:55 +0300

    fix bash to work on windows (only windows for now)

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
-
 local wezterm = require 'wezterm';
 return {
   font = wezterm.font("Roboto Mono"),
   hide_tab_bar_if_only_one_tab = true,
-  default_prog = { "bash", "--login", "-i" }
+  default_domain = "local",
+  default_prog = { "C:/Program Files/Git/bin/bash.exe", "--login", "-i" }
 }
