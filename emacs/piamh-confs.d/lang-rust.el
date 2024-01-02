a5b2de0525d7cd032b5c3fcd96657376af123186
Niv Pagir <nivpgir@gmail.com>
2024-01-02 20:28:01 +0200

    emacs - cleanup and fixes of rustic mode

--- emacs/piamh-confs.d/lang-rust.el
+++ emacs/piamh-confs.d/lang-rust.el
@@ -15,24 +15,11 @@
 ;;   :config
 ;;   (setq lsp-eldoc-hook nil)
 ;;   (setq lsp-rust-analyzer-server-display-inlay-hints t))
-(use-package lsp-mode)
-(use-package lsp-ui)
+
 (use-package rustic
-  :straight t
-  ;; :hook
-  ;; (rustic-mode . electric-pair-local-mode)
-  :general
-  (:keymaps 'rustic-mode-map
-	    "C-c C-c l"  'flycheck-list-errors
-	    ;; "C-c C-c a"  'lsp-execute-code-action
-	    ;; "C-c C-c r"  'lsp-rename
-	    ;; "C-c C-c q"  'lsp-workspace-restart
-	    ;; "C-c C-c Q"  'lsp-workspace-shutdown
-	    ;; "C-c C-c s"  'lsp-rust-analyzer-status
-	    )
-  ;; :config
-  ;; (setq lsp-eldoc-hook nil)
-  ;; (setq lsp-rust-analyzer-server-display-inlay-hints t))
+  :custom
+  (rustic-lsp-client 'eglot)
+  :hook (rustic-mode . (lambda () (flymake-mode -1)))
   )
 (use-package rust-playground
   :straight t)
