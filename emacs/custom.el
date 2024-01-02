92204c5786a5bc836abc78cc2709ae7b3b7b7afe
Niv Pagir <nivpgir@gmail.com>
2024-01-02 20:09:35 +0200

    emacs - suppress iedit warnings

--- emacs/custom.el
+++ emacs/custom.el
@@ -7,7 +7,7 @@
    '("02591317120fb1d02f8eb4ad48831823a7926113fa9ecfb5a59742420de206e0" default))
  '(inhibit-startup-screen t)
  '(lsp-rust-analyzer-server-command "rust-analyzer" nil nil "Customized with use-package rustic")
- '(warning-suppress-types '((comp))))
+ '(warning-suppress-types '((iedit) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
