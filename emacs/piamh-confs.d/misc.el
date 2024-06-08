35b79e9a0ee09a66d182569fbd9e7c191db62b9e
Niv Pagir <nivpgir@gmail.com>
2024-06-08 15:22:59 +0300

    remove emacs-eat recipe

--- emacs/piamh-confs.d/misc.el
+++ emacs/piamh-confs.d/misc.el
@@ -19,14 +19,14 @@
 (use-package environ)
 
 (use-package eat
-  :straight '(eat :type git
-		  :host codeberg
-		  :repo "akib/emacs-eat"
-		  :files ("*.el" ("term" "term/*.el") "*.texi"
-			  "*.ti" ("terminfo/e" "terminfo/e/*")
-			  ("terminfo/65" "terminfo/65/*")
-			  ("integration" "integration/*")
-			  (:exclude ".dir-locals.el" "*-tests.el")))
+  ;; :straight '(eat :type git
+  ;; 		  :host codeberg
+  ;; 		  :repo "akib/emacs-eat"
+  ;; 		  :files ("*.el" ("term" "term/*.el") "*.texi"
+  ;; 			  "*.ti" ("terminfo/e" "terminfo/e/*")
+  ;; 			  ("terminfo/65" "terminfo/65/*")
+  ;; 			  ("integration" "integration/*")
+  ;; 			  (:exclude ".dir-locals.el" "*-tests.el")))
   :custom
   (eat-term-scrollback-size nil)
   :init
