6edd92e10608b49fdcb9bd2b94216f4cc8145a6a
Niv Pagir <nivpgir@gmail.com>
2026-01-24 20:03:27 +0200

    add claude code plugin

--- emacs/piamh-confs.d/llm.el
+++ emacs/piamh-confs.d/llm.el
@@ -18,3 +18,18 @@
     "l r" 'gptel-rewrite
     )
   )
+
+(use-package claude-code
+  :straight (:type git :host github :repo "stevemolitor/claude-code.el" :branch "main" :depth 1
+                   :files ("*.el" (:exclude "images/*")))
+  :bind-keymap
+  ("C-c c" . claude-code-command-map) ;; or your preferred key
+  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
+  :bind
+  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode))
+  :config
+  ;; optional IDE integration with Monet
+  (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
+  (monet-mode 1)
+
+  (claude-code-mode))
