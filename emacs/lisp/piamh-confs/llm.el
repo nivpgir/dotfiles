8261e35f0efd71fc8c44d3afa621c8f772180863
Niv Pagir <nivpgir@gmail.com>
2026-05-10 01:55:49 +0300

    emacs - add agent-shell configuration

--- emacs/lisp/piamh-confs/llm.el
+++ emacs/lisp/piamh-confs/llm.el
@@ -19,38 +19,8 @@
     )
   )
 
-
-(use-package inheritenv
-  :straight (:type git :host github :repo "purcell/inheritenv"))
-
-
-
-(use-package monet
-  :straight  (:type git
-		    :host github
-		    :repo "stevemolitor/monet")
-
-  :custom
-  (monet-diff-tool #'monet-ediff-tool)
-  (monet-diff-cleanup-tool #'monet-ediff-cleanup-tool)
-  :hook
-  ('claude-code-process-environment-functions . 'monet-start-server-function)
+(use-package agent-shell
+  :straight t
   )
-
-
-(use-package claude-code
-  :straight (:type git :host github :repo "stevemolitor/claude-code.el" :branch "main" :depth 1
-                   :files ("*.el" (:exclude "images/*")))
-  :bind-keymap
-  ("C-c c" . claude-code-command-map) ;; or your preferred key
-  ;; Optionally define a repeat map so that "M" will cycle thru Claude auto-accept/plan/confirm modes after invoking claude-code-cycle-mode / C-c M.
-  :bind
-  (:repeat-map my-claude-code-map ("M" . claude-code-cycle-mode))
-  :config
-  ;; optional IDE integration with Monet
-  (add-hook 'claude-code-process-environment-functions #'monet-start-server-function)
-  (monet-mode 1)
-
-  (claude-code-mode))
 
 (provide 'piamh-confs/llm)
